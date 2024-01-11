02c0e2b8cdc33986c95fe1bac681fcdb7b8b054a
Thomas Boresky <tbtboresky3@gmail.com>
2024-01-11 12:00:22 -0500

    Completed JavaScript sectin

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,18 +1,27 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-/*
-var topic = "Git";
-if(topic === 'HTML'){
-    console.log("Let's Study HTML!");
-}else if(topic === 'CSS'){
-    console.log("Let's study CSS!");
-}else if(topic === 'Git'){
-    console.log("Let's Study Git!");
-}else if(topic === 'JavaScript'){
-    console.log("Let's study JavaScript!");
-}else{
-    console.log('Please Try Again!');
-}
-*/
-for(var x = 0; x < topics.length; x++){
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function ListTopics(){
+    for(var x = 0; x < topics.length; x++){
     console.log(topics[x]);
+    }
 }
+
+function SelectTopic(){
+    if(randomTopic === 'HTML'){
+        console.log("Let's Study HTML!");
+    }else if(randomTopic === 'CSS'){
+        console.log("Let's study CSS!");
+    }else if(randomTopic === 'Git'){
+        console.log("Let's Study Git!");
+    }else if(randomTopic === 'JavaScript'){
+        console.log("Let's study JavaScript!");
+    }else{
+        console.log('Please Try Again!');
+        }
+}        
+
+console.log('Here are the topics we studied in the Prework:');
+ListTopics();
+console.log('Which Topic Should We Study First?');
+SelectTopic();
